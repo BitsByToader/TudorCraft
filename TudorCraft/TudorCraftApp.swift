e71c96a2fb107041a54b5a33b7622c85d6767645
Ifrim Tudor <ifrimtudor@protonmail.com>
2022-11-02 22:41:01 +0200

    Initial Commit

---
+++ TudorCraft/TudorCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TudorCraftApp.swift
+//  TudorCraft
+//
+//  Created by Tudor Ifrim on 02.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TudorCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
